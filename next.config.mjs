ab5f1b8d245fe835e5acc33674212f80bbacfdad
pawan-meena <pawanmeenazee@outlook.com>
2024-02-20 21:35:53 +0530

    token initiated

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
+import { config } from "dotenv";
+
+config();
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
-
 export default nextConfig;
--- src/lib/sanity.js
+++ src/lib/sanity.js
@@ -3,6 +3,8 @@ const { createClient } = require("@sanity/client");
 const sanityClient = createClient({
   projectId: "xh1730zu", // you can find this in sanity.json
   dataset: "production", // or the name you chose in sanity.json
+  apiVersion: '2024-02-20',
+  oken: process.env.SANITY_TOKEN,
   useCdn: false, // `false` if you want to ensure fresh data
 });
 
